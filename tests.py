6efd9e265144d62fcd793092a1c5206b888a9779
Philip Kimmey <philip.kimmey@gmail.com>
2011-12-29 21:49:41 -0800

    Add negative case for attribute selector (as simply ignoring the attribute selector would be sufficient with the old test case)

--- tests.py
+++ tests.py
@@ -343,10 +343,17 @@ class ComplexSelectors(unittest.TestCase):
         output = Pynliner().from_string(html).with_cssString(css).run()
         self.assertEqual(output, expected)
 
-    def test_attribute_selector(self):
+    def test_attribute_selector_match(self):
         html = """<h1 title="foo">Hello World!</h1>"""
         css = """h1[title="foo"] { color: red; }"""
         expected = u"""<h1 title="foo" style="color: red">Hello World!</h1>"""
+        output = Pynliner().from_string(html).with_cssString(css).run()
+        self.assertEqual(output, expected)
+    
+    def test_attribute_selector_no_match(self):
+        html = """<h1 title="bar">Hello World!</h1>"""
+        css = """h1[title="foo"] { color: red; }"""
+        expected = u"""<h1 title="bar">Hello World!</h1>"""
         output = Pynliner().from_string(html).with_cssString(css).run()
         self.assertEqual(output, expected)
 
