81f711d67e0b9b86df2767743ebe124295dbb78f
J. Tanner Netterville <tannern@gmail.com>
2013-04-23 18:31:24 -0500

    Convert to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='pynliner',
       version='0.4.0',
-      description='Python CSS-to-inline-styles conversion tool for HTML using BeautifulSoup and cssutils',
+      description='Python CSS-to-inline-styles conversion tool for HTML using'
+                  ' BeautifulSoup and cssutils',
       author='Tanner Netterville',
       author_email='tannern@gmail.com',
       packages=('pynliner',),
-      requires=('BeautifulSoup(<4.0)',
-                'cssutils(>=0.9.7)',
-                ),
-      provides=('pynliner',),
-     )
+      install_requires=('BeautifulSoup<4.0',
+                        'cssutils>=0.9.7',
+      ),
+      provides=('pynliner',)
+)
