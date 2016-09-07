6d093c5750b2f61616a8647808b563248af515cd
Villiers Strauss <villiers.strauss@gmail.com>
2016-09-07 16:37:27 +0200

    Fix duplicate element issue

--- pynliner/__init__.py
+++ pynliner/__init__.py
@@ -217,27 +217,30 @@ class Pynliner(object):
         for rule in rules:
             for selector in rule.selectorList:
                 for element in select(self.soup, selector.selectorText):
-                    if element not in elem_prop_map:
-                        elem_prop_map[element] = []
-                    elem_prop_map[element].append({
+                    element_tuple = (element, id(element))
+                    if element_tuple not in elem_prop_map:
+                        elem_prop_map[element_tuple] = []
+                    elem_prop_map[element_tuple].append({
                         'specificity': selector.specificity,
                         'props': rule.style.getProperties(),
                     })
 
         # build up another property list using selector specificity
-        for elem, props in elem_prop_map.items():
-            if elem not in elem_style_map:
-                elem_style_map[elem] = cssutils.css.CSSStyleDeclaration()
+        for elem_tuple, props in elem_prop_map.items():
+            elem, elem_id = elem_tuple
+            if elem_tuple not in elem_style_map:
+                elem_style_map[elem_tuple] = cssutils.css.CSSStyleDeclaration()
             # ascending sort of prop_lists based on specificity
             props = sorted(props, key=lambda p: p['specificity'])
             # for each prop_list, apply to CSSStyleDeclaration
             for prop_list in map(lambda obj: obj['props'], props):
                 for prop in prop_list:
-                    elem_style_map[elem].removeProperty(prop.name)
-                    elem_style_map[elem].setProperty(prop.name, prop.value)
+                    elem_style_map[elem_tuple].removeProperty(prop.name)
+                    elem_style_map[elem_tuple].setProperty(prop.name, prop.value)
 
         # apply rules to elements
-        for elem, style_declaration in elem_style_map.items():
+        for elem_tuple, style_declaration in elem_style_map.items():
+            elem, elem_id = elem_tuple
             if elem.has_attr('style'):
                 elem['style'] = u'%s; %s' % (style_declaration.cssText.replace('\n', ' '), elem['style'])
             else:
