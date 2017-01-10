9406d56988236f0a3ecef9f0c62c93289b091354
Tanner Netterville <tannern@gmail.com>
2017-01-10 13:35:46 -0600

    version bump to 0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import setup
 
 setup(name='pynliner',
-      version='0.8.0rc1',
+      version='0.8.0',
       description='Python CSS-to-inline-styles conversion tool for HTML using'
                   ' BeautifulSoup and cssutils',
       author='Tanner Netterville',
